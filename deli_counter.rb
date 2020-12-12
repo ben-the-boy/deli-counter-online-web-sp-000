bc23b67ec1fd27cc196a6098ba29a0c01b321bec
Ben Hayes <b.t.hayes@comcast.net>
2020-12-12 20:21:45 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,21 @@
-# Write your code here.
+def line(array)
+  if array.size == 0
+    puts "The line is currently empty."
+  else
+    new_array = []
+    array.each.with_index(1) do |value, index|
+      new_array << "#{index}. #{value}"
+    end
+    queue = new_array.join(" ")
+    puts "The line is currently: #{queue}"
+  end
+end
+
+def take_a_number(array, name)
+  new_array = []
+  array << name 
+  array.each.with_index(1) do |person, index|
+    new_array << "#{index}"
+  end
+  puts "Welcome, #{name}. You are currently #{new_array[-1]} in line."
+end
